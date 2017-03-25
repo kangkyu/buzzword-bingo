6b67f612534c923f297afb9f9c0139bf924dc9a6
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-03-24 19:16:24 -0700

    Rendering HTML + type annotations

--- Bingo.elm
+++ Bingo.elm
@@ -1,17 +1,50 @@
 module Bingo exposing (..)
 
-import Html
+import Html exposing (Html, text, h2, h1, a, footer, header, div)
+import Html.Attributes exposing (id, class, href)
 
 
+playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
 
 
-playerInfoText name gameNumber =
-    playerInfo name gameNumber
-        |> String.toUpper
-        |> Html.text
+viewPlayer : String -> Int -> Html msg
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2 [ id "info", class "classy" ]
+            [ playerInfoText ]
 
 
+viewHeader : String -> Html msg
+viewHeader title =
+    header []
+        [ h1 [] [ text title ]
+        ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org" ]
+            [ text "Powered by Elm" ]
+        ]
+
+
+view : Html msg
+view =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer "Mike" 3
+        , viewFooter
+        ]
+
+
+main : Html msg
 main =
-    playerInfoText "Mike" 3
+    view
